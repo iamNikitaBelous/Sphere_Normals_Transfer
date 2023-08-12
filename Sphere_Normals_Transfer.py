715add44ccc7e3938f0ad0443e32f8bb6148ce96
iamNikitaBelous <80423911+iamNikitaBelous@users.noreply.github.com>
2023-08-12 12:54:27 +0300

    Update Sphere_Normals_Transfer.py
    Blender Python script for transfering normals from sphere to tree.

--- Sphere_Normals_Transfer.py
+++ Sphere_Normals_Transfer.py
@@ -1,3 +1,15 @@
+# README! and Please, save all of files before running the script!
+# 
+# 1. Hide everything except the tree (tree don't have to be one object)
+# 2. Select all of visible object and set any active
+# 3. Run the script!
+#
+# Tree must have separated material for leaves!
+# Tree must be only visible object in scene!
+# Don't have to deselect object's mesh
+# Don't have to set specific active object
+# Tree may have other groups and modifiers
+
 import bpy
 
 def Sphere_Normals_Transfer():
